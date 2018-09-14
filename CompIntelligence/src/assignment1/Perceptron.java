2b920a2d389d2bde7d13c6de26edb63618b24ccc
Tim Polderdijk <t.l.m.polderdijk@student.tudelft.nl>
2018-09-14 11:15:15 +0200

    Generalise perceptron code

--- CompIntelligence/src/assignment1/Perceptron.java
+++ CompIntelligence/src/assignment1/Perceptron.java
@@ -6,37 +6,49 @@ import java.util.Random;
 public class Perceptron {
 	
 	public static void main(String[] args) {
-		long seed = 0;
-		Random random = new Random(seed);
 		int[][] input = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
-		int[] desired = {0, 0, 0, 1};
-		int[] predicted = new int[4];
+		int[] desired = {0, 1, 1, 1};
+		int epochs = 30;
 		double alpha = 0.1;
-		int epochs = 10;
-		double[] MSE = new double[epochs];
-		
-		double[] weights = new double[2];
 		double threshold = 0.2;
-		weights[0] = random.nextDouble() - 0.5;
-		weights[1] = random.nextDouble() - 0.5;
-		
+	}
+
+	public void train(int[][] input, int[] output, int epochs, double alpha, double threshold) {
+		long seed = 0;
+		Random random = new Random();
+		int rows = input.length;
+		int cols = input[0].length;
+		int[] predicted = new int[rows];
+		double[] MSE = new double[epochs];
+		double[] weights = new double[cols];
+
+		for (int i = 0; i < cols; i++) {
+			double offset = random.nextDouble();
+			weights[i] = offset =0.5;
+		}
+
 		for (int n = 0; n < epochs; n++) {
-			
-			for (int i = 0; i < input.length; i++) {
+
+			for (int i = 0; i < rows; i++) {
+				double sum = 0;
+				for(int k = 0; k < cols; k++) {
+					sum += input[i][k]*weights[k];
+				}
 				
-				if (input[i][0] * weights[0] + input[i][1] * weights[1] - threshold >= 0) {
+				if (sum - threshold >= 0) {
 					predicted[i] = 1;
 				}
 				else {
 					predicted[i] = 0;
 				}	
 				
-				double error = desired[i] - predicted[i];
-				MSE[n] += error*error; 
-				weights[0] += alpha * input[i][0] * error;
-				weights[1] += alpha * input[i][1] * error;
+				double error = output[i] - predicted[i];
+				MSE[n] += error*error;
+				for (int j = 0; j < cols ; j++) {
+					weights[j] += alpha * input[i][j] * error;
+				}
 			}
-			MSE[n] = MSE[n] / input.length;
+			MSE[n] = MSE[n] / rows;
 			System.out.println(MSE[n]);	
 		}
 		// System.out.println(Arrays.toString(MSE));
