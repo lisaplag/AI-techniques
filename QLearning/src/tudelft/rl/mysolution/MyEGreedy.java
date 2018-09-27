3746c21a092f5857f7d3ac400ea261a7e27b395b
Lisa Plag <l.l.m.plag@student.tudelft.nl>
2018-09-27 10:00:55 +0200

    Getting best action

--- QLearning/src/tudelft/rl/mysolution/MyEGreedy.java
+++ QLearning/src/tudelft/rl/mysolution/MyEGreedy.java
@@ -1,5 +1,7 @@
 package tudelft.rl.mysolution;
 
+import java.util.ArrayList;
+
 import tudelft.rl.Action;
 import tudelft.rl.Agent;
 import tudelft.rl.EGreedy;
@@ -16,8 +18,27 @@ public class MyEGreedy extends EGreedy {
 
 	@Override
 	public Action getBestAction(Agent r, Maze m, QLearning q) {
-		//TODO to select the best possible action currently known in State s.
-		return null;
+		
+		//get information
+		State s = m.getState(r.x, r.y);
+		ArrayList<javax.swing.Action> validActions = m.getValidActions(r);
+		double[] actionValues = getActionValues(s, validActions);
+		
+		//initialize best action to first valid action
+		Action bestAction = validActions.get(0);
+		double bestActionValue = actionValues[0];
+		
+		//loop through possible actions in State s
+		for(int i = 1; i < validActions.size(); i++) {
+			
+			if(actionValues[i] > bestActionValue) {
+				//update best action
+				bestAction = validActions.get(i);
+				bestActionValue = actionValues[i];				
+			}
+		}
+		
+		return bestAction;
 	}
 
 	@Override
